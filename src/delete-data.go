7e674333d86c2355b5ca9ea29d7ac3b05ca6b6d4
karanr1990 <33133108+karanr1990@users.noreply.github.com>
2020-03-20 13:45:50 +0530

    Create delete-data.go

---
+++ src/delete-data.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "root"
+	dbname   = "golangdb"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	sqlStatement := `DELETE FROM users WHERE id = $1;`
+	_, err = db.Exec(sqlStatement, 14)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("record deleted")
+}
