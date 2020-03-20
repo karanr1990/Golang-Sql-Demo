85a868a7695961072a12ee4aa0ebba522e58bf4d
karanr1990 <33133108+karanr1990@users.noreply.github.com>
2020-03-20 15:16:43 +0530

    Create query-multiple-rows.go

---
+++ src/query-multiple-rows.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "root"
+	dbname   = "golangdb"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, first_name FROM users LIMIT $1", 4)
+	if err != nil {
+		// handle this error better than this
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var firstName string
+		err = rows.Scan(&id, &firstName)
+		if err != nil {
+			// handle this error
+			panic(err)
+		}
+		fmt.Println(id, firstName)
+	}
+	// get any error encountered during iteration
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("return multiple rows")
+}
